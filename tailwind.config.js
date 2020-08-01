e0965fc027d04da402d8033d931d6dac8e05ca7a
Emily <12156026+emilyjt@users.noreply.github.com>
2020-08-01 21:58:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  prefix: 'tw-',
+  purge: {
+    content: ["src/**/*.{php,html}"],
+    enabled: false,
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
