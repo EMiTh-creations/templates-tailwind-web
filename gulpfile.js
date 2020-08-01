1ec09dc36689d827309d578b589c844575d3866f
Emily <12156026+emilyjt@users.noreply.github.com>
2020-08-01 23:12:29 +0100

    updating folder refrences

--- gulpfile.js
+++ gulpfile.js
@@ -7,123 +7,132 @@ var cssmqpacker = require("css-mqpacker");
 var pngquant = require("imagemin-pngquant");
 var mozjpeg = require("imagemin-mozjpeg");
 var tailwindcss = require("tailwindcss");
-var browserSync = require('browser-sync').create();
+var browserSync = require("browser-sync").create();
 
 // define source and destination paths
 var paths = {
-    styles: {
-        src: "src/scss/**/*.scss",
-        dest: "dist/css",
-        tailwind: {
-            src: "src/tailwind/**/*.css",
-            dest: "dist/css"
-        }
+  styles: {
+    src: "./src/scss/**/*.scss",
+    dest: "./dist/css",
+    tailwind: {
+      src: "./src/tailwind/**/*.css",
+      dest: "./dist/css",
     },
-    scripts: {
-        src: "src/js/**/*.js",
-        dest: "dist/js"
-    },
-    images: {
-        src: "src/images/**/*.{jpg,jpeg,png,svg}",
-        dest: "dist/images"
-    },
-    static: {
-        src: "src/**/*.{php,html,ini,htaccess,xml,ico,json}",
-        dest: "dist"
-    }
+  },
+  scripts: {
+    src: "./src/js/**/*.js",
+    dest: "./dist/js",
+  },
+  images: {
+    src: "./src/images/**/*.{jpg,jpeg,png,svg}",
+    dest: "./dist/images",
+  },
+  static: {
+    src: "./src/**/*.{php,html,ini,htaccess,xml,ico,json}",
+    dest: "./dist",
+  },
 };
 
 // set the image compression options
 var imgOptions = [
-    mozjpeg({
-        quality: "86"
-    }),
-    pngquant({
-        quality: [0.7, 0.8]
-    })
+  mozjpeg({
+    quality: "86",
+  }),
+  pngquant({
+    quality: [0.7, 0.8],
+  }),
 ];
 
 // Styles Processing function. Check for changes, process SASS
 // minify, group media queries and remove unused selectors
 function styles() {
-    return gulp
-        .src(paths.styles.src)
-        .pipe(plugins.changed(paths.styles.dest))
-        .pipe(plugins.sass())
-        .pipe(plugins.postcss([autoprefixer(), cssnano(), cssmqpacker()]))
-        .pipe(gulp.dest(paths.styles.dest));
+  return gulp
+    .src(paths.styles.src)
+    .pipe(plugins.changed(paths.styles.dest))
+    .pipe(plugins.sass())
+    .pipe(plugins.postcss([autoprefixer(), cssnano(), cssmqpacker()]))
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
 // Styles Processing function. Check for changes, process SASS
 // minify, group media queries and remove unused selectors
 function styles_tailwind() {
-    return gulp
-        .src(paths.styles.tailwind.src)
-        .pipe(plugins.postcss([tailwindcss(), autoprefixer(), cssnano()]))
-        .pipe(gulp.dest(paths.styles.tailwind.dest));
+  return gulp
+    .src(paths.styles.tailwind.src)
+    .pipe(plugins.postcss([tailwindcss(), autoprefixer(), cssnano()]))
+    .pipe(gulp.dest(paths.styles.tailwind.dest));
 }
 
 // cleaning function. Will usually only be run before build.
 function clean() {
-    return del(["dist/.*", "dist/**/*"]).then(paths => {
-        console.log("Deleted files and folders:\n", paths.join("\n"));
-    });
+  return del(["./dist/.*", "./dist/**/*"]).then((paths) => {
+    console.log("Deleted files and folders:\n", paths.join("\n"));
+  });
 }
 
 // concatenate and uglify javascript
 function scripts() {
-    return gulp
-        .src(paths.scripts.src)
-        .pipe(plugins.changed(paths.scripts.dest))
-        .pipe(plugins.concat("main.js"))
-        .pipe(plugins.terser())
-        .pipe(gulp.dest(paths.scripts.dest));
+  return gulp
+    .src(paths.scripts.src)
+    .pipe(plugins.changed(paths.scripts.dest))
+    .pipe(plugins.concat("main.js"))
+    .pipe(plugins.terser())
+    .pipe(gulp.dest(paths.scripts.dest));
 }
 
 // processing images
 function images() {
-    return gulp
-        .src(paths.images.src)
-        .pipe(plugins.changed(paths.images.dest))
-        .pipe(plugins.imagemin(imgOptions, {
-            verbose: true
-        }))
-        .pipe(gulp.dest(paths.images.dest));
+  return gulp
+    .src(paths.images.src)
+    .pipe(plugins.changed(paths.images.dest))
+    .pipe(
+      plugins.imagemin(imgOptions, {
+        verbose: true,
+      })
+    )
+    .pipe(gulp.dest(paths.images.dest));
 }
 
 // copy static files, specifically including dot files like .htaccess.
 function static() {
-    return gulp
-        .src(paths.static.src, {
-            dot: true
-        })
-        .pipe(plugins.changed(paths.static.dest))
-        .pipe(gulp.dest(paths.static.dest));
+  return gulp
+    .src(paths.static.src, {
+      dot: true,
+    })
+    .pipe(plugins.changed(paths.static.dest))
+    .pipe(gulp.dest(paths.static.dest));
 }
 
 function browser() {
-    browserSync.init({
-        server: "./dist"
-    });
+  browserSync.init({
+    server: "./dist",
+  });
 
-    gulp.watch("./dist/**/*").on("change", browserSync.reload);
+  gulp.watch("./dist/**/*").on("change", browserSync.reload);
 }
 
 // watch task
 function watch() {
-    gulp.watch(paths.scripts.src, scripts);
-    gulp.watch(paths.styles.src, styles);
-    gulp.watch([paths.styles.tailwind.src, "tailwind.config.js"], styles_tailwind);
-    gulp.watch(paths.images.src, images);
-    gulp.watch(paths.static.src, static);
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(
+    [paths.styles.tailwind.src, "./tailwind.config.js"],
+    styles_tailwind
+  );
+  gulp.watch(paths.images.src, images);
+  gulp.watch(paths.static.src, static);
 }
 
 //define build process, whether tasks run in parallel or series.
-var build = gulp.series(clean, static, gulp.parallel(styles, styles_tailwind, scripts, images));
+var build = gulp.series(
+  clean,
+  static,
+  gulp.parallel(styles, styles_tailwind, scripts, images)
+);
 
 //define standalone tasks
 gulp.task("styles", styles);
-gulp.task("styles_tailwind", styles_tailwind)
+gulp.task("styles_tailwind", styles_tailwind);
 gulp.task("clean", clean);
 gulp.task("scripts", scripts);
 gulp.task("images", images);
@@ -131,7 +140,7 @@ gulp.task("static", static);
 
 gulp.task("watch", watch);
 gulp.task("browser", browser);
-gulp.task("browser-watch", gulp.series(build, gulp.parallel(watch, browser)))
+gulp.task("browser-watch", gulp.series(build, gulp.parallel(watch, browser)));
 
 //define build and also set default task as build
 gulp.task("build", build);
